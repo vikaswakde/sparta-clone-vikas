3ecf0542010933f7d09c6013f38a61ede2795a64
Vikas Wakde <110342308+vikaswakde@users.noreply.github.com>
2024-03-05 04:28:29 +0530

    gradient changes made

--- src/components/Manage.jsx
+++ src/components/Manage.jsx
@@ -12,8 +12,8 @@ const Managment = () => {
         />
 
         {/* gradient start */}
-        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
-        <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient" />
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full red__gradient" />
+        <div className="absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full white__gradient" />
         {/* gradient end */}
       </div>
 
